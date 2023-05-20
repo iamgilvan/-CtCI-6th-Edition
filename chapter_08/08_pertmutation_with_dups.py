89cec1d877ad33f3952efbefb79e01dc790d7c35
Gilvan Almeida <gilvan.almeida@version1.com>
2023-05-20 16:03:43 +0100

    Recursion and Dynamic Programming: Permutation With Dups

---
+++ chapter_08/08_pertmutation_with_dups.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+def permutation_with_dups(letters):
+    return permutations("", sorted(letters))
+
+def permutations(partial, letters):
+    if len(letters) == 0:
+        return [partial]
+    permutation = []
+    previous_letter = None
+    for i, letter in enumerate(letters):
+        if letter == previous_letter:
+            continue
+        next_partial = partial + letter
+        letter_before = letters[:i]
+        letter_after = letters[i+1:]
+        next_letter = letter_before + letter_after
+        permutation += permutations(next_partial, next_letter)
+        previous_letter = letter
+    return permutation
+
+class Test(unittest.TestCase):
+  def test_permutations(self):
+    self.assertEqual(permutation_with_dups("abca"), ["aabc", "aacb", "abac", "abca",
+        "acab", "acba", "baac", "baca", "bcaa", "caab", "caba", "cbaa"])
+    self.assertEqual(permutation_with_dups("ABCD"), ["ABCD", "ABDC", "ACBD", "ACDB",
+        "ADBC", "ADCB", "BACD", "BADC", "BCAD", "BCDA", "BDAC", "BDCA",
+        "CABD", "CADB", "CBAD", "CBDA", "CDAB", "CDBA", "DABC", "DACB",
+        "DBAC", "DBCA", "DCAB", "DCBA"])
+    self.assertListEqual(permutation_with_dups("aab"), ['aab', 'aba', 'baa'])
+
+if __name__ == "__main__":
+  unittest.main()
